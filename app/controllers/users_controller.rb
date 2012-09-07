d068ed7dcd8708e8d13abe57691d25518bdf930e
AlexeiZenovich <a.zenovich@hotmail.com>
2012-09-07 09:36:56 +0300

    check bag with chnge user role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
 
   def create
     @user = user.new(params[:user])
+    @user.role = :user
     flash[:notice] = "User was successfully created." if @user.save
     respond_with(@user)
   end
 
   def update
+    @user.role = params[:user][:role] if can? :change_role, @user
     flash[:notice] = "Update was successfully." if @user.update_attributes(params[:user])
     respond_with @user
   end
@@ -18,6 +20,4 @@ class UsersController < ApplicationController
     flash[:notice] = "Successfully destroyed advertisement."
     respond_with @user
   end
-
-
 end
